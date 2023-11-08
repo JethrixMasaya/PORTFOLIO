9e05498d42f7d8d3e66035442596e643000332d6
xparklingwater <67900822+xparklingwater@users.noreply.github.com>
2023-11-08 17:23:03 +0800

    Initial commit

---
+++ js/ultjs.js
@@ -0,0 +1,9 @@
+window.addEventListener("load", function() {
+  const loading = document.querySelector(".loading");
+  loading.className += " hidden";
+});
+
+window.addEventListener("load", function() {
+  const loading = document.querySelector(".loadgos");
+  loading.className += " hidden";
+});
